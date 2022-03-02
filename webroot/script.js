cb45086252e26394210b05ff6b8269f8d47d41b6
Arenarxi <iock891030@gmail.com>
2022-03-02 20:54:00 +0800

    first commit

---
+++ webroot/script.js
@@ -0,0 +1,11 @@
+const coobut = document.getElementById("dj 8 click")
+
+const coo1 = document.getElementById("dj 8_1")
+const coo2 = document.getElementById("dj 8_2")
+const cooA = document.getElementById("cooA")
+
+
+coobut.addEventListener("click", function () {
+    console.log("啟啥小?")
+    coo2.style.color = "#22C32Eff";
+})
